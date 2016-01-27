23c14d0892f76538601050d3d56c92bface07582
Adam Slater <adam.slater.1010@gmail.com>
2016-01-27 11:45:27 -0800

    fixed link for the first item (zzz)

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -28,6 +28,14 @@ class First extends Application {
         $this->render();
     }
 
+    function zzz()
+    {
+        $this->data['pagebody'] = 'justone';	// this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->first();
+        $this->data = array_merge($this->data, $source);
+        $this->render();
+    }
 }
 
 /* End of file First.php */
