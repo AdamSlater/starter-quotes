a2fb47d1d12c06e228d69194171e29f20a8ed422
Adam Slater <adam.slater.1010@gmail.com>
2016-01-27 12:49:28 -0800

    fixed the link for the last person (bingo)

--- application/config/routes.php
+++ application/config/routes.php
@@ -42,5 +42,7 @@ $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 $route['sleep'] = 'first/zzz';
 $route['show/(:num)'] = 'first/gimme/$1';
+$route['([a-z]{4})/bingo'] = 'bingo';
+
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
